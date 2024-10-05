ba76823e2344736f031bc57f3552c99a6b01be1b
luan640 <luan640@gmail.com>
2024-10-05 11:17:35 -0300

    Remover chave exposta do código

--- myapp/functions_gpt.py
+++ myapp/functions_gpt.py
@@ -2,7 +2,7 @@ import openai
 import requests
 import json
 
-openai.api_key = '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'  # Substitua pela sua chave correta
+# openai.api_key = aqui seu token
 
 def tratar_numero_wa(wa_id):
     # Verifique se o número começa com o código de país +55 (Brasil)
